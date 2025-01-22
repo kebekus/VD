6eff7bfe1b7dfab309ef3887841e0d6b77c6ff57
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-01-22 08:12:22 +0100

    Update meromorphicAt.lean

--- VD/ToMathlib/meromorphicAt.lean
+++ VD/ToMathlib/meromorphicAt.lean
@@ -22,37 +22,40 @@ theorem MeromorphicAt.order_mul
   (hf.mul hg).order = hf.order + hg.order := by
   -- Trivial cases: one of the functions vanishes around z₀
   by_cases h₂f : hf.order = ⊤
-  · simp [hf.order_of_locallyZero_mul_meromorphic hg h₂f, h₂f]
+  · simp [hf.order_of_locallyZero_mul_meromorphic hg, h₂f]
   by_cases h₂g : hg.order = ⊤
   · have : f * g = g * f := by simp_rw [mul_comm]
-    simp [this, hg.order_of_locallyZero_mul_meromorphic hf h₂g, h₂g]
+    simp [this, hg.order_of_locallyZero_mul_meromorphic hf, h₂g]
 
   -- Non-trivial case: both functions do not vanish around z₀
-  have h₃f₁ := (WithTop.coe_untop hf.order h₂f).symm
-  have h₃f₂ := (WithTop.coe_untop hg.order h₂g).symm
-  obtain ⟨F, h₁F, h₂F, h₃F⟩ := (hf.order_eq_int_iff (hf.order.untop h₂f)).1 h₃f₁
-  obtain ⟨G, h₁G, h₂G, h₃G⟩ := (hg.order_eq_int_iff (hg.order.untop h₂g)).1 h₃f₂
-  rw [h₃f₁, h₃f₂, ← WithTop.coe_add, MeromorphicAt.order_eq_int_iff]
-  use F * G, h₁F.mul h₁G
+  have h₃f := (WithTop.coe_untop hf.order h₂f).symm
+  have h₃g := (WithTop.coe_untop hg.order h₂g).symm
+  obtain ⟨F, h₁F, h₂F, h₃F⟩ := (hf.order_eq_int_iff (hf.order.untop h₂f)).1 h₃f
+  obtain ⟨G, h₁G, h₂G, h₃G⟩ := (hg.order_eq_int_iff (hg.order.untop h₂g)).1 h₃g
+  rw [h₃f, h₃g, ← WithTop.coe_add, MeromorphicAt.order_eq_int_iff]
+  use F * G, h₁F.mul h₁G, by simp; tauto
+  rw [eventually_nhdsWithin_iff, eventually_nhds_iff] at *
+  obtain ⟨t₁, h₁t₁, h₂t₁, h₃t₁⟩ := h₃F
+  obtain ⟨t₂, h₁t₂, h₂t₂, h₃t₂⟩ := h₃G
+  use t₁ ∩ t₂
   constructor
-  · simp; tauto
-  · rw [eventually_nhdsWithin_iff, eventually_nhds_iff] at *
-    obtain ⟨t₁, h₁t₁, h₂t₁, h₃t₁⟩ := h₃F
-    obtain ⟨t₂, h₁t₂, h₂t₂, h₃t₂⟩ := h₃G
-    use t₁ ∩ t₂
-    constructor
-    · intro y h₁y h₂y
-      simp [h₁t₁ y h₁y.1 h₂y, h₁t₂ y h₁y.2 h₂y, zpow_add' (by left; exact sub_ne_zero_of_ne h₂y)]
-      group
-    · exact ⟨IsOpen.inter h₂t₁ h₂t₂, Set.mem_inter h₃t₁ h₃t₂⟩
+  · intro y h₁y h₂y
+    simp [h₁t₁ y h₁y.1 h₂y, h₁t₂ y h₁y.2 h₂y, zpow_add' (by left; exact sub_ne_zero_of_ne h₂y)]
+    group
+  · exact ⟨IsOpen.inter h₂t₁ h₂t₂, Set.mem_inter h₃t₁ h₃t₂⟩
 
 
 /-- The order multiplies by `n` when taking an analytic function to its `n`th power -/
 theorem MeromorphicAt.order_pow (hf : MeromorphicAt f z₀) {n : ℕ} :
     (hf.pow n).order = n * hf.order := by
   induction' n with n hn
-  · simp [AnalyticAt.order_eq_zero_iff]
+  · simp
+    rw [← WithTop.coe_zero, MeromorphicAt.order_eq_int_iff]
+    use 1, analyticAt_const
+    simp
   · simp [add_mul, pow_add, (hf.pow n).order_mul hf, hn]
+
+    sorry
 
 
 -- TODO: `order_zpow` is not yet ported to mathlib
