c4808e2260856a960d1cdf43be26ea1d0dde3b72
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-02-07 17:08:17 +0100

    Working...

--- VD/analyticAt_preimgCodiscrete.lean
+++ VD/analyticAt_preimgCodiscrete.lean
@@ -2,19 +2,19 @@ import Mathlib.Analysis.Analytic.IsolatedZeros
 import Mathlib.Analysis.Analytic.Order
 import Mathlib.Order.Filter.Bases
 import Mathlib.Order.Filter.EventuallyConst
+import VD.codiscreteWithin
 
 open Filter Topology Set
 
 variable {𝕜 : Type*} [NontriviallyNormedField 𝕜] {E : Type*} [NormedAddCommGroup E]
   [NormedSpace 𝕜 E] {s : E} {p q : FormalMultilinearSeries 𝕜 𝕜 E} {f g : 𝕜 → E} {n : ℕ} {z z₀ : 𝕜}
 
-
-theorem yy {x : 𝕜} {f : 𝕜 → E}
-    (hf : AnalyticAt 𝕜 f x)
-    (h₂f : ¬Filter.EventuallyConst f (𝓝 x)) :
-    (𝓝[≠] x).map f ≤ (𝓝[≠] f x) := by
-  intro s hs
-  simp only [mem_map]
+/-- Preimages of codiscrete sets, local version: if `f` is analytic at `x` and
+  not locally constant, then the preimage of any punctured neighbourhood of
+  `f x` is a punctured neighbourhood of `x`. -/
+theorem AnalyticAt.preimg_of_puncNhd {x : 𝕜} {f : 𝕜 → E} {s : Set E} (hf : AnalyticAt 𝕜 f x)
+    (h₂f : ¬Filter.EventuallyConst f (𝓝 x)) (hs : s ∈ 𝓝[≠] f x) :
+    f ⁻¹' s ∈ 𝓝[≠] x := by
   have : insert (f x) s ∈ 𝓝 (f x) := by
     rw [mem_nhds_iff]
     obtain ⟨u, h₁u, h₂u, h₃u⟩ := mem_nhdsWithin.1 hs
@@ -44,18 +44,41 @@ theorem yy {x : 𝕜} {f : 𝕜 → E}
   tauto
   fun_prop
 
-theorem zz {f : 𝕜 → E} (hf : AnalyticOnNhd 𝕜 f ⊤) :
-    Filter.map f (Filter.codiscrete 𝕜) ≤ (Filter.codiscrete E) := by
-  intro s hs
-  simp only [mem_map]
-  rw [mem_codiscrete]
-  intro x
-  rw [disjoint_principal_right, compl_compl]
-  simp_rw [mem_codiscrete, disjoint_principal_right] at hs
-  simp at hs
-  let y := f x
-  have hy : y ∈ (⊤ : Set E) := by sorry
-  let h₂y := hs y
+/-- Preimages of codiscrete sets, local filter version: if `f` is analytic at
+  `x` and not locally constant, then the push-forward of the punctured
+  neighbourhood filter `𝓝[≠] x` is less than or equal to the punctured
+  neighbourhood filter `𝓝[≠] f x`. -/
+theorem AnalyticAt.map_of_puncNhdFilter {x : 𝕜} {f : 𝕜 → E} (hf : AnalyticAt 𝕜 f x)
+    (h₂f : ¬Filter.EventuallyConst f (𝓝 x)) :
+    (𝓝[≠] x).map f ≤ (𝓝[≠] f x) := fun _ hs ↦ mem_map.1 (preimg_of_puncNhd hf h₂f hs)
 
-  let ZZ := yy hf h₂y
-  sorry
+/-- Preimages of codiscrete sets: if `f` is analytic on a neighbourhood of `U`
+  and not locally constant, then the preimage of any subset that codiscrete within `f '' U`
+  is codiscrete within `U`.
+
+  Applications will almost certainly employ the theorem `Filter.codiscreteWithin.mono`.
+-/
+theorem AnalyticOnNhd.preimg_codiscrete {U : Set 𝕜} {s : Set E} {f : 𝕜 → E}
+    (hf : AnalyticOnNhd 𝕜 f U) (h₂f : ∀ x ∈ U, ¬EventuallyConst f (𝓝 x)) (hs : s ∈ codiscreteWithin (f '' U)):
+    f ⁻¹' s ∈ codiscreteWithin U := by
+  simp_rw [mem_codiscreteWithin, disjoint_principal_right, Set.compl_diff] at *
+  intro x hx
+  let A := (hf x hx).preimg_of_puncNhd (h₂f x hx) (hs (f x) (by tauto))
+  have : f ⁻¹' (s ∪ (f '' U)ᶜ) ⊆  f ⁻¹' s ∪ Uᶜ := by
+    rw [preimage_union, preimage_compl]
+    refine union_subset_union_right (f ⁻¹' s) ?_
+    intro x hx
+    simp_all
+    tauto
+  exact mem_of_superset A this
+
+/-- Preimages of codiscrete sets, filter version: if `f` is analytic on a neighbourhood of `U`
+  and not locally constant, then the push-forward of the filter of sets codiscrete within `U` is
+  less than or equal to the filter of sets codiscrete within `f '' U`.
+
+  Applications will almost certainly employ the theorem `Filter.codiscreteWithin.mono`.
+-/
+theorem AnalyticOnNhd.map_of_codiscreteWithinFilter {U : Set 𝕜} {f : 𝕜 → E} (hf : AnalyticOnNhd 𝕜 f U)
+    (h₂f : ∀ x ∈ U, ¬EventuallyConst f (𝓝 x)) :
+    Filter.map f (Filter.codiscreteWithin U) ≤ (Filter.codiscreteWithin (f '' U)) :=
+  fun _ hs ↦ mem_map.1 (preimg_codiscrete hf h₂f hs)
--- VD/codiscreteWithin.lean
+++ VD/codiscreteWithin.lean
@@ -3,15 +3,16 @@ import Mathlib.Topology.DiscreteSubset
 --
 theorem Set.compl_diff {α : Type u} {s t : Set α} : (t \ s)ᶜ = s ∪ tᶜ := by
   ext z
-  simp
+  rw [mem_compl_iff, mem_diff, not_and, not_not, mem_union]
   tauto
 --
 
+/-- Codiscreteness within `U` depends only on the intersection with `U`. -/
 theorem codiscreteWithin_congr_inter
     {X : Type u_1} [TopologicalSpace X]
-    {S T U : Set X}
-    (hST : S ∩ U = T ∩ U) :
-    S ∈ Filter.codiscreteWithin U ↔ T ∈ Filter.codiscreteWithin U := by
+    {S₁ S₂ U : Set X}
+    (hST : S₁ ∩ U = S₂ ∩ U) :
+    S₁ ∈ Filter.codiscreteWithin U ↔ S₂ ∈ Filter.codiscreteWithin U := by
   rw [mem_codiscreteWithin, mem_codiscreteWithin, ← Set.diff_inter_self_eq_diff, hST,
     Set.diff_inter_self_eq_diff]
 
@@ -26,12 +27,10 @@ theorem discreteTopology_of_codiscreteWithin
   rw [← Filter.mem_iff_inf_principal_compl, ← Set.compl_diff]
   simp_all only [h x, Set.compl_union, compl_compl, Set.mem_inter_iff, Set.mem_compl_iff]
 
+/-- If a set is codiscrete with `U`, then it is codiscrete within any subset of `U`. -/
 theorem Filter.codiscreteWithin.mono {X : Type u_1} [TopologicalSpace X] {U₁ U₂ : Set X}
     (hU : U₁ ⊆ U₂) :
     (Filter.codiscreteWithin U₁) ≤ (Filter.codiscreteWithin U₂) := by
   intro s hs
   simp_rw [mem_codiscreteWithin, disjoint_principal_right] at hs ⊢
-  intro x hx
-  apply mem_of_superset (hs x (hU hx))
-  rw [Set.compl_subset_compl]
-  exact fun y hy ↦ ⟨hU hy.1, hy.2⟩
+  exact fun x hx ↦ mem_of_superset (hs x (hU hx)) (Set.compl_subset_compl.2 (fun y hy ↦ ⟨hU hy.1, hy.2⟩))
