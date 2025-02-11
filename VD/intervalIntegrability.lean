642e5afd31953a1a77c424ed6765a62e92e3c9fc
Stefan Kebekus <kebekus@users.noreply.github.com>
2025-02-11 07:16:59 +0100

    add file

--- VD/intervalIntegrability.lean
+++ VD/intervalIntegrability.lean
@@ -4,6 +4,7 @@ import Mathlib.MeasureTheory.Integral.IntervalAverage
 import Mathlib.MeasureTheory.Integral.Periodic
 import VD.ToMathlib.analyticAt_preimgCodiscrete
 import VD.ToMathlib.codiscreteWithin
+import VD.ToMathlib.intervalIntegrability
 
 open scoped Interval Topology
 open Real Filter MeasureTheory intervalIntegral
@@ -11,70 +12,21 @@ open Real Filter MeasureTheory intervalIntegral
 
 /- Integral and Integrability up to changes on codiscrete sets -/
 
-theorem d
-  {U S : Set ℂ}
-  {c : ℂ}
-  {r : ℝ}
-  (hr : r ≠ 0)
-  (hU : Metric.sphere c |r| ⊆ U)
-  (hS : S ∈ Filter.codiscreteWithin U) :
-  Countable ((circleMap c r)⁻¹' Sᶜ) := by
-
-  have : (circleMap c r)⁻¹' (S ∪ Uᶜ)ᶜ = (circleMap c r)⁻¹' Sᶜ := by
-    simp [(by simpa : (circleMap c r)⁻¹' U = ⊤)]
-  rw [← this]
-
-  apply Set.Countable.preimage_circleMap _ c hr
-  have : DiscreteTopology ((S ∪ Uᶜ)ᶜ : Set ℂ) := by
-    rw [discreteTopology_subtype_iff]
-    rw [mem_codiscreteWithin] at hS; simp at hS
-
-    intro x hx
-    rw [← mem_iff_inf_principal_compl, (by ext z; simp; tauto : S ∪ Uᶜ = (U \ S)ᶜ)]
-    rw [Set.compl_union, compl_compl] at hx
-    exact hS x hx.2
-
-  apply TopologicalSpace.separableSpace_iff_countable.1
-  exact TopologicalSpace.SecondCountableTopology.to_separableSpace
-
-
-theorem integrability_congr_changeDiscrete₀
-  {f₁ f₂ : ℂ → ℝ}
-  {U : Set ℂ}
-  {r : ℝ}
-  (hU : Metric.sphere 0 |r| ⊆ U)
-  (hf : f₁ =ᶠ[Filter.codiscreteWithin U] f₂) :
-  IntervalIntegrable (f₁ ∘ (circleMap 0 r)) MeasureTheory.volume 0 (2 * π) → IntervalIntegrable (f₂ ∘ (circleMap 0 r)) MeasureTheory.volume 0 (2 * π) := by
-  intro hf₁
-
-  by_cases hr : r = 0
-  · unfold circleMap
-    rw [hr]
-    simp
-    have : f₂ ∘ (fun (θ : ℝ) ↦ 0) = (fun r ↦ f₂ 0) := by
-      exact rfl
-    rw [this]
-    simp
-
-  · apply IntervalIntegrable.congr hf₁
-    rw [Filter.eventuallyEq_iff_exists_mem]
-    use (circleMap 0 r)⁻¹' {z | f₁ z = f₂ z}
-    constructor
-    · apply Set.Countable.measure_zero (d hr hU hf)
-    · tauto
-
-
 theorem integrability_congr_changeDiscrete
-  {f₁ f₂ : ℂ → ℝ}
-  {U : Set ℂ}
-  {r : ℝ}
-  (hU : Metric.sphere (0 : ℂ) |r| ⊆ U)
-  (hf : f₁ =ᶠ[Filter.codiscreteWithin U] f₂) :
+    {f₁ f₂ : ℂ → ℝ} {U : Set ℂ} {r : ℝ}
+    (hU : Metric.sphere (0 : ℂ) |r| ⊆ U)
+    (hR : r ≠ 0)
+    (hf : f₁ =ᶠ[Filter.codiscreteWithin U] f₂) :
   IntervalIntegrable (f₁ ∘ (circleMap 0 r)) MeasureTheory.volume 0 (2 * π) ↔ IntervalIntegrable (f₂ ∘ (circleMap 0 r)) MeasureTheory.volume 0 (2 * π) := by
-
   constructor
-  · exact integrability_congr_changeDiscrete₀ hU hf
-  · exact integrability_congr_changeDiscrete₀ hU (EventuallyEq.symm hf)
+  · apply (intervalIntegrable_congr_codiscreteWithin _).1
+    filter_upwards [Filter.codiscreteWithin.mono (by tauto : (Ι 0 (2 * π)) ⊆ Set.univ)
+      (circleMap_preimg_codiscrete hR (Filter.codiscreteWithin.mono hU hf))]
+    tauto
+  · apply (intervalIntegrable_congr_codiscreteWithin _).1
+    filter_upwards [Filter.codiscreteWithin.mono (by tauto : (Ι 0 (2 * π)) ⊆ Set.univ)
+      (circleMap_preimg_codiscrete hR (Filter.codiscreteWithin.mono hU hf.symm))]
+    tauto
 
 
 theorem integral_congr_changeDiscrete
@@ -85,7 +37,6 @@ theorem integral_congr_changeDiscrete
   (hU : Metric.sphere 0 |r| ⊆ U)
   (hf : f₁ =ᶠ[Filter.codiscreteWithin U] f₂) :
   ∫ (x : ℝ) in (0)..(2 * π), f₁ (circleMap 0 r x) = ∫ (x : ℝ) in (0)..(2 * π), f₂ (circleMap 0 r x) := by
-
   apply intervalIntegral.integral_congr_ae
   rw [eventually_iff_exists_mem]
   use (circleMap 0 r)⁻¹' {z | f₁ z = f₂ z}
@@ -142,85 +93,3 @@ theorem integrabl_congr_negRadius
   rw [X]
   simp
   rw [add_comm]
-
-theorem ae_of_restrVol_le_codiscreteWithin {U : Set ℝ} (hU : MeasurableSet U) :
-    (MeasureTheory.ae (volume.restrict U)) ≤ (Filter.codiscreteWithin U) := by
-  intro s hs
-  have := discreteTopology_of_codiscreteWithin hs
-  rw [mem_ae_iff, Measure.restrict_apply' hU]
-  apply Set.Countable.measure_zero (TopologicalSpace.separableSpace_iff_countable.1
-    TopologicalSpace.SecondCountableTopology.to_separableSpace)
-
-theorem intervalIntegrable_congr_codiscreteWithin
-    {E : Type u_3} [NormedAddCommGroup E] {a b : ℝ} {f₁ f₂ : ℝ → E} (hf : f₁ =ᶠ[Filter.codiscreteWithin (Ι a b)] f₂) :
-    IntervalIntegrable f₁ MeasureTheory.volume a b ↔
-      IntervalIntegrable f₂ MeasureTheory.volume a b := by
-  constructor
-  · intro hf₁
-    apply hf₁.congr
-    rw [Filter.eventuallyEq_iff_exists_mem] at *
-    obtain ⟨s, h₁s, h₂s⟩ := hf
-    use s, ae_of_restrVol_le_codiscreteWithin measurableSet_Ioc h₁s, h₂s
-  · rw [eventuallyEq_comm] at hf
-    intro hf₁
-    apply hf₁.congr
-    rw [Filter.eventuallyEq_iff_exists_mem] at *
-    obtain ⟨s, h₁s, h₂s⟩ := hf
-    use s, ae_of_restrVol_le_codiscreteWithin measurableSet_Ioc h₁s, h₂s
-
-open Complex in
-/-- The circleMap is real analytic. -/
-theorem analyticOnNhd_circleMap {c : ℂ} {R : ℝ} :
-    AnalyticOnNhd ℝ (circleMap c R) Set.univ := by
-  intro z hz
-  unfold circleMap
-  apply analyticAt_const.add
-  apply analyticAt_const.mul
-  rw [(by rfl : (fun θ ↦ exp (↑θ * I) : ℝ → ℂ) = cexp ∘ (fun θ ↦ (↑θ * I) : ℝ → ℂ))]
-  apply analyticAt_cexp.restrictScalars.comp ((ofRealCLM.analyticAt z).mul (by fun_prop))
-
-theorem circleMap_preimg_codiscrete {c : ℂ} {R : ℝ} (hR : R ≠ 0) :
-    Filter.map (circleMap c R) (Filter.codiscrete ℝ) ≤ (Filter.codiscreteWithin (Metric.sphere c |R|)) := by
-  intro s hs
-  apply analyticOnNhd_circleMap.preimg_codiscrete
-  · intro x hx
-    by_contra hCon
-    obtain ⟨a, ha⟩ := eventuallyConst_iff_exists_eventuallyEq.1 hCon
-    have := ha.deriv.eq_of_nhds
-    simp [hR] at this
-  · rwa [Set.image_univ, range_circleMap]
-
-theorem circleIntegrable_congr_codiscreteWithin
-    {c : ℂ} {R : ℝ} {f₁ f₂ : ℂ → ℂ} (hf : f₁ =ᶠ[Filter.codiscreteWithin (Metric.sphere c |R|)] f₂) (hR : R ≠ 0) :
-    CircleIntegrable f₁ c R → CircleIntegrable f₂ c R := by
-  apply (intervalIntegrable_congr_codiscreteWithin _).1
-  rw [Filter.eventuallyEq_iff_exists_mem]
-  use (circleMap c R)⁻¹' {z | f₁ z = f₂ z}, Filter.codiscreteWithin.mono
-    (by simp) (circleMap_preimg_codiscrete hR hf), (by tauto)
-
-theorem intervalIntegral.integral_congr_ae_restict
-    {a b : ℝ} {f g : ℝ → E} {μ : Measure ℝ}
-    [NormedAddCommGroup E] [NormedSpace ℝ E]
-    (h : f =ᵐ[μ.restrict (Ι a b)] g) :
-    ∫ x in a..b, f x ∂μ = ∫ x in a..b, g x ∂μ :=
-  intervalIntegral.integral_congr_ae (ae_imp_of_ae_restrict h)
-
-theorem intervalIntegral_congr_codiscreteWithin
-    {a b : ℝ} {f₁ f₂ : ℝ → ℝ} (hf : f₁ =ᶠ[Filter.codiscreteWithin (Ι a b)] f₂) :
-    ∫ (x : ℝ) in a..b, f₁ x = ∫ (x : ℝ) in a..b, f₂ x :=
-  integral_congr_ae_restict (ae_of_restrVol_le_codiscreteWithin measurableSet_uIoc hf)
-
-theorem intervalAverage_congr_codiscreteWithin
-    {a b : ℝ} {f₁ f₂ : ℝ → ℝ} (hf : f₁ =ᶠ[Filter.codiscreteWithin (Ι a b)] f₂) :
-  ⨍ (x : ℝ) in a..b, f₁ x = ⨍ (x : ℝ) in a..b, f₂ x := by
-  rw [interval_average_eq, intervalIntegral_congr_codiscreteWithin hf,
-    ← interval_average_eq]
-
-theorem circleIntegral_congr_codiscreteWithin
-    {c : ℂ} {R : ℝ} {f₁ f₂ : ℂ → ℂ} (hf : f₁ =ᶠ[Filter.codiscreteWithin (Metric.sphere c |R|)] f₂) (hR : R ≠ 0) :
-    (∮ z in C(c, R), f₁ z) = (∮ z in C(c, R), f₂ z) := by
-  apply integral_congr_ae_restict
-  apply ae_of_restrVol_le_codiscreteWithin measurableSet_uIoc
-  simp only [deriv_circleMap, smul_eq_mul, mul_eq_mul_left_iff, mul_eq_zero,
-    circleMap_eq_center_iff, hR, Complex.I_ne_zero, or_self, or_false]
-  exact Filter.codiscreteWithin.mono (by tauto) (circleMap_preimg_codiscrete hR hf)
